f11c74a8888fba3a5f4eadaa95f5583a6809940d
Jon McDonald <jon@onewebcentric.com>
2012-09-28 16:51:57 -0600

    IQE
    Updates to IQE

--- functions.php
+++ functions.php
@@ -35,21 +35,41 @@ if(file_exists(TT_INCLUDES_FOLDER_PATH . 'auto_install/auto_install.php')){
 include_once (TT_INCLUDES_FOLDER_PATH . 'auto_install/auto_install.php'); // sample data insert file
 }
 
-// Hook in
-add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+/**
+ * Adds height gracefully to the form, I will be enqueueing a light jQuery that will require a valid number
+ *
+ * By Jonathon McDonald :: 09/28/2012
+ */
+
+// Hook into add the field to billing
+add_filter( 'woocommerce_checkout_fields' , 'jon_add_custom_checkout_fields' );
  
 // Our hooked in function - $fields is passed via the filter!
-function custom_override_checkout_fields( $fields ) {
+function jon_add_custom_checkout_fields( $fields ) {
      $fields['billing']['billing_height'] = array(
         'label'     => __('Height', 'woocommerce'),
     'placeholder'   => _x('Height', 'placeholder', 'woocommerce'),
     'required'  => true,
-    'class'     => array('form-row-wide'),
+    'class'     => array('form-row-first'),
     'clear'     => true
      );
  
      return $fields;
 }
 
+add_action('wp_enqueue_scripts', 'jon_jquery_modal');
 
+function jon_jquery_modal()
+{
+	wp_enqueue_script(
+		'simplemodal',
+		get_template_directory_uri() . '/js/jquery.simplemodal-1.4.3.js',
+		array('jquery')
+	);
+
+	wp_enqueue_script(
+		'jonmodal',
+		get_template_directory_uri() . '/js/jon-modal.js',
+		array('jquery'));
+}
 ?>
--- ship.js
+++ ship.js
@@ -1,20 +1,5 @@
 jQuery(document).ready(function($) {
-	$shipaddress = $('.shipping_address');
-	$ship_status = $('#shiptobilling-checkbox');
-
-	if($('#shiptobilling-checkbox').is(':checked'))
-	{
-		$('.shipping_address').hide();
-	}
-
-	$('#shiptobilling-checkbox').click( function() {
-		if($('#shiptobilling-checkbox').is(':checked'))
-		{
-			return $('.shipping_address').hide();
-		}
-		else
-		{
-			return $('.shipping_address').show();
-		}
+	$(".openModal").click(function (e) {
+		$("#hiddenContent").modal();
 	});
 });
